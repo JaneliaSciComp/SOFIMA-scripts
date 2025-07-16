8b828a5774f4de780d0014675ed375d63360171d
Ben Arthur <arthurb@hhmi.org>
2025-07-16 09:38:20 -0400

    make data loading code a plugin

--- em-alignment.sh
+++ em-alignment.sh
@@ -1,13 +1,14 @@
-# ./em-alignment.sh <patch-size> <stride> <batch-size>
+# ./em-alignment.sh <data-loader> <patch-size> <stride> <batch-size>
 
 basepath=/groups/scicompsoft/home/arthurb/projects/multi-sem
-logfile=$basepath/em-alignment-$1-$2-$3.log
+logfile=$basepath/em-alignment-$1-$2-$3-$4.log
 jobid_regex='Job <\([0-9]*\)> '
 
-cmd="conda run -n multi-sem python $basepath/em-alignment1.py $1 $2 $3"
+# -n9 needed for 160,32,64 and 150k^2 data
+cmd="conda run -n multi-sem python $basepath/em-alignment1.py $1 $2 $3 $4"
 out=`echo $cmd | bsub -Psema -n12 -gpu "num=1" -q gpu_a100 -W 1440 -o $logfile`
 jobid=`expr match "$out" "$jobid_regex"`
 dep=done\($jobid\)
 
-cmd="conda run -n multi-sem python $basepath/em-alignment2.py $1 $2"
+cmd="conda run -n multi-sem python $basepath/em-alignment2.py $1 $2 $3"
 echo $cmd | bsub -Psema -n32 -W 1440 -w "$dep" -o $logfile
