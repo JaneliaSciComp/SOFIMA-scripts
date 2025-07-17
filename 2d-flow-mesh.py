2b8fdbd836c66b110ddb8394df843ff695527e23
Ben Arthur <arthurb@hhmi.org>
2025-07-17 12:54:49 -0400

    do not save unnecessary 2d meshes

--- 2d-flow-mesh.py
+++ 2d-flow-mesh.py
@@ -51,10 +51,10 @@ flow = np.array(flow)[np.newaxis,:]
 
 # Convert to [channels, z, y, x].
 flow = np.transpose(flow, [1, 0, 2, 3])
-pad = patch_size // 2 // stride
 
 # Pad to account for the edges of the images where there is insufficient
 # context to estimate flow.
+pad = patch_size // 2 // stride
 flow = np.pad(flow, [[0, 0], [0, 0], [pad, pad], [pad, pad]], constant_values=np.nan)
 
 # remove uncertain flow estimates by replacing them with NaNs
@@ -62,12 +62,6 @@ t0 = time.time()
 flow_clean = flow_utils.clean_flow(flow, min_peak_ratio=1.6, min_peak_sharpness=1.6,
                                    max_magnitude=80, max_deviation=20)
 print("clean_flow took", time.time() - t0, "sec")
-
-
-#f, ax = plt.subplots(1, 2, figsize=(10, 5))
-#ax[0].imshow(flow[0, 0, ...], cmap=plt.cm.RdBu, vmin=-10, vmax=10)
-#ax[1].imshow(flow_clean[0, 0, ...], cmap=plt.cm.RdBu, vmin=-10, vmax=10)
-
 
 ### multi-resolution flow fields would be merged here
 
@@ -78,22 +72,12 @@ config = mesh.IntegrationConfig(dt=0.001, gamma=0.0, k0=0.01, k=0.1, stride=(str
                                 stop_v_max=0.005, dt_max=1000, start_cap=0.01,
                                 final_cap=10, prefer_orig_order=True)
 
-solved = [np.zeros_like(flow_clean[:, 0:1, ...])]
-origin = jnp.array([0., 0.])
-
-prev = map_utils.compose_maps_fast(flow_clean[:, 0:1, ...], origin, stride,
-                                   solved[-1], origin, stride)
-
-x = np.zeros_like(solved[0])
+solved = np.zeros_like(flow_clean)
 
 # also uses GPU
 t0 = time.time()
-x, e_kin, num_steps = mesh.relax_mesh(x, prev, config)
+solved, e_kin, num_steps = mesh.relax_mesh(solved, flow_clean, config)
 print("relax_mesh took", time.time() - t0, "sec")
-
-x = np.array(x)
-solved.append(x)
-solved = np.concatenate(solved, axis=1)
 
 params = '.patch'+str(patch_size)+'.stride'+str(stride)
 
