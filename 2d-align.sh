18667fd966a8b4fd4d5a98a71cdc1952f0f56164
Ben Arthur <arthurb@hhmi.org>
2025-09-16 16:25:10 -0400

    change conda env name

--- 2d-align.sh
+++ 2d-align.sh
@@ -1,14 +1,14 @@
 # ./2d-align.sh <data-loader> <patch-size> <stride> <batch-size>
 
-basepath=/groups/scicompsoft/home/arthurb/projects/multi-sem
+basepath=/groups/scicompsoft/home/arthurb/projects/multisem
 logfile=$basepath/2d-align-$1-$2-$3-$4.log
 jobid_regex='Job <\([0-9]*\)> '
 
 # -n9 needed for 160,32,64 and 150k^2 data
-cmd="conda run -n multi-sem python $basepath/em-alignment1.py $1 $2 $3 $4"
+cmd="conda run -n multisem python $basepath/2d-flow-mesh.py $1 $2 $3 $4"
 out=`echo $cmd | bsub -Psema -n12 -gpu "num=1" -q gpu_a100 -W 1440 -o $logfile`
 jobid=`expr match "$out" "$jobid_regex"`
 dep=done\($jobid\)
 
-cmd="conda run -n multi-sem python $basepath/em-alignment2.py $1 $2 $3"
+cmd="conda run -n multisem python $basepath/2d-invmap.py $1 $2 $3"
 echo $cmd | bsub -Psema -n32 -W 1440 -w "$dep" -o $logfile
