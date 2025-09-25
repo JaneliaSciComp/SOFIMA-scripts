eae5caede2421307eeba75ec7916cd76a9c98b21
Ben Arthur <arthurb@hhmi.org>
2025-09-25 12:58:18 -0400

    hoist basepath

--- 1-plane-stitch.sh
+++ 1-plane-stitch.sh
@@ -2,8 +2,8 @@
 
 # launches a cluster job to stitch tiles in a bunch of planes
 
-# usage: ./1-plane-stitch.sh <level> <patch_size> <stride> <min_z> <max_z>
-# e.g. ./1-plane-stitch.sh 0 50 10 10770 10780
+# usage: ./1-plane-stitch.sh <level> <patch_size> <stride> <min_z> <max_z> <outpath>
+# e.g. ./1-plane-stitch.sh 0 50 5 10770 10780 /nrs/cellmap/arthurb/stitch.patch50.stride5
 
 level=$1
 patch_size=$2
@@ -13,7 +13,8 @@ url='http://em-services-1.int.janelia.org:8080/render-ws/v1/owner/cellmap/projec
 MIN_Z=$4
 MAX_Z=$5
 
-outpath=/nrs/cellmap/arthurb
+outpath=$6
+
 bsub_flags=(-Pcellmap -n1 -gpu "num=1" -q gpu_l4)
 
 tiles=$(curl "$url/zRange/${MIN_Z},${MAX_Z}/layoutFile?format=SCHEFFER" | tail -n +2 | cut -d' ' -f2)
@@ -24,5 +25,5 @@ for slice in "${uniq_slices[@]}" ; do
     logfile=$outpath/$(basename $slice).log
     bsub ${bsub_flags[@]} -oo $logfile \
             conda run -n multi-sem --no-capture-output \
-            python -u ./1-plane-stitch.py "data-aphid-1-plane" $slice $level $patch_size $stride
+            python -u ./1-plane-stitch.py "data-aphid-1-plane" $slice $level $patch_size $stride $outpath
 done
--- N-planes-align.sh
+++ N-planes-align.sh
@@ -3,47 +3,48 @@
 # launches dependent cluster jobs for each step needed to align a stack of N planes
 
 # usage: ./N-planes-align.sh <data-loader> <min-z> <max-z> <patch-size> <stride> <batch-size>
-# e.g. ./N-planes-align.sh "data-aphid-N-planes" 10770 10780 100 20 4096
+# e.g. ./N-planes-align.sh "data-aphid-N-planes" /nrs/cellmap/arthurb/stitch.patch50.stride5 10770 10780 100 5 4096
 
 data_loader=$1
-min_z=$2
-max_z=$3
-patch_size=$4
-stride=$5
-batch_size=$6
+basepath=$2
+min_z=$3
+max_z=$4
+patch_size=$5
+stride=$6
+batch_size=$7
 
 params=minz${min_z}.maxz${max_z}.patch${patch_size}.stride${stride}
 
 jobid_regex='Job <\([0-9]*\)> '
 
-bsub_flags=(-Pcellmap -n4 -gpu "num=1" -q gpu_l4)
-logfile=/nrs/cellmap/arthurb/flow.${params}.log
+bsub_flags=(-Pcellmap -n1 -gpu "num=1" -q gpu_l4)
+logfile=$basepath/flow.${params}.log
 bsub_stdout=`bsub ${bsub_flags[@]} -oo $logfile \
     conda run -n multi-sem --no-capture-output \
-    python -u ./N-planes-flow.py $data_loader $min_z $max_z $patch_size $stride $batch_size`
+    python -u ./N-planes-flow.py $data_loader $basepath $min_z $max_z $patch_size $stride $batch_size`
 jobid=`expr match "$bsub_stdout" "$jobid_regex"`
 dependency=done\($jobid\)
 
 bsub_flags=(-Pcellmap -n1 -gpu "num=1" -q gpu_l4)
-logfile=/nrs/cellmap/arthurb/mesh.${params}.log
+logfile=$basepath/mesh.${params}.log
 bsub_stdout=`bsub ${bsub_flags[@]} -oo $logfile -w $dependency \
     conda run -n multi-sem --no-capture-output \
-    python -u ./N-planes-mesh.py $data_loader $min_z $max_z $patch_size $stride $batch_size`
+    python -u ./N-planes-mesh.py $data_loader $basepath $min_z $max_z $patch_size $stride $batch_size`
 jobid=`expr match "$bsub_stdout" "$jobid_regex"`
 dependency=done\($jobid\)
 
 bsub_flags=(-Pcellmap -n2)
-logfile=/nrs/cellmap/arthurb/invmap.${params}.log
+logfile=$basepath/invmap.${params}.log
 bsub_stdout=`bsub ${bsub_flags[@]} -oo $logfile -w $dependency \
     conda run -n multi-sem --no-capture-output \
-   python -u ./N-planes-invmap.py $data_loader $min_z $max_z $patch_size $stride`
+   python -u ./N-planes-invmap.py $data_loader $basepath $min_z $max_z $patch_size $stride`
 jobid=`expr match "$bsub_stdout" "$jobid_regex"`
 dependency=done\($jobid\)
 
-bsub_flags=(-Pcellmap -n8)
-logfile=/nrs/cellmap/arthurb/warp.${params}.log
+bsub_flags=(-Pcellmap -n4)
+logfile=$basepath/warp.${params}.log
 bsub_stdout=`bsub ${bsub_flags[@]} -oo $logfile -w $dependency \
     conda run -n multi-sem --no-capture-output \
-    python -u ./N-planes-warp.py $data_loader $min_z $max_z $patch_size $stride`
+    python -u ./N-planes-warp.py $data_loader $basepath $min_z $max_z $patch_size $stride`
 jobid=`expr match "$bsub_stdout" "$jobid_regex"`
 dependency=done\($jobid\)
