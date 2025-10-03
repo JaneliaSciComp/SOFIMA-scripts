ecf777ba5c6ebac672a9ecbe3689b6d2c4684861
Ben Arthur <arthurb@hhmi.org>
2025-10-03 17:55:01 -0400

    fix bug when warping more than chunk_size planes

--- N-planes-warp.py
+++ N-planes-warp.py
@@ -53,12 +53,11 @@ warped = np.empty((chunk_size, *warped0.shape), dtype=warped0.dtype)
 warped[0,...] = warped0
 fid = data.open_warp([len(filenames_noext), *warped0.shape], chunk_size, basepath, params)
 
-futures = []
+data_box = bounding_box.BoundingBox(start=(0, 0, 0), size=[*np.flip(np.array(warped0.shape)),1])
+out_box = bounding_box.BoundingBox(start=(0, 0, 0), size=[*np.flip(np.array(warped0.shape)),1])
 
 for z in range(1, len(filenames_noext)):
   print(datetime.now(), 'z =', z)
-  data_box = bounding_box.BoundingBox(start=(0, 0, 0), size=[*np.flip(np.array(warped0.shape)),1])
-  out_box = bounding_box.BoundingBox(start=(0, 0, 0), size=[*np.flip(np.array(warped0.shape)),1])
 
   curr = np.transpose(np.expand_dims(np.expand_dims(data.load_data(basepath, filenames_noext, z,0),
                                                     axis=-1),
@@ -67,12 +66,9 @@ for z in range(1, len(filenames_noext)):
   warped[z % chunk_size, ...] = warp.warp_subvolume(curr, data_box,
       invmap[:, z:z+1, ...], boxMx, stride_min, out_box, 'lanczos', parallelism=1)[0, 0, ...]
   if z % chunk_size == chunk_size - 1:
-      futures.append(data.write_warp_planes(fid, warped, z-chunk_size+1, z+1))
+      print(datetime.now(), 'writing chunk plane', z//chunk_size)
+      data.write_warp_planes(fid, warped, z-chunk_size+1, z+1).result()
   elif z == len(filenames_noext) - 1:
-      futures.append(data.write_warp_planes(fid, warped[:z % chunk_size + 1],
-                                            z//chunk_size * chunk_size, z+1))
-
-print(datetime.now(), 'waiting for writing to finish')
-while len(futures)>0:
-  print(datetime.now(), len(futures), 'chunk planes remaining')
-  futures.pop(0).result()
+      print(datetime.now(), 'writing last chunk plane')
+      data.write_warp_planes(fid, warped[:z % chunk_size + 1],
+                             z//chunk_size * chunk_size, z+1).result()
