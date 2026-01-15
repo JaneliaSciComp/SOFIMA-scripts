1fa5c74b467b4730b5368c27d76e3933e4368a79
Ben Arthur <arthurb@hhmi.org>
2026-01-15 12:25:55 -0500

    parameters top, bot in 2-planes and add warp to shell script

--- 2-planes-align.sh
+++ 2-planes-align.sh
@@ -1,22 +1,38 @@
-# ./2-planes-align.sh <data-loader> <basepath> <patch-size> <stride> <batch-size>
+# ./2-planes-align.sh <data-loader> <basepath> <patch-size> <stride> <batch-size> <chunk>
+
+# ./2-planes-align.sh data-hess-2-planes /nrs/hess/data/hess_wafers_60_61/export/zarr_datasets/surface-align/run_20251219_110000/pass03-scale2 flat-w61_serial_080_to_089-w61_s080_r00-top-face.zarr flat-w61_serial_070_to_079-w61_s079_r00-bot-face.zarr 160 8 1024 1024
 
 data_loader=$1
 basepath=$2
-patch_size=$3
-stride=$4
-batch_size=$5
+top=$3
+bot=$4
+patch_size=$5
+stride=$6
+batch_size=$7
+chunk=$8
 
 params=patch${patch_size}.stride${stride}
 
 jobid_regex='Job <\([0-9]*\)> '
 
-# -n9 needed for 160,32,64 and 150k^2 data
-logfile=$basepath/2-planes-flow-mesh-$params.log
-cmd="conda run -n multisem python ./2-planes-flow-mesh.py $data_loader $basepath $patch_size $stride $batch_size"
-out=`echo $cmd | bsub -Phess -n12 -gpu "num=1" -q gpu_a100 -W 1440 -o $logfile`
-jobid=`expr match "$out" "$jobid_regex"`
-dep=done\($jobid\)
+bsub_flags=(-Phess -n4 -gpu "num=1" -q gpu_l4 -W 1440)
+logfile=$basepath/flow-mesh-$params.log
+bsub_stdout=`bsub ${bsub_flags[@]} -oo $logfile \
+    conda run -n multi-sem --no-capture-output \
+    python -u ./2-planes-flow-mesh.py $data_loader $basepath $top $bot $patch_size $stride $batch_size`
+jobid=`expr match "$bsub_stdout" "$jobid_regex"`
+dependency=done\($jobid\)
 
-logfile=$basepath/2-planes-invmap-$params.log
-cmd="conda run -n multisem python ./2-planes-invmap.py $data_loader $basepath $patch_size $stride"
-echo $cmd | bsub -Phess -n32 -W 1440 -w "$dep" -o $logfile
+bsub_flags=(-Phess -n32 -W 1440)
+logfile=$basepath/invmap-$params.log
+bsub_stdout=`bsub ${bsub_flags[@]} -w "$dependency" -oo $logfile \
+    conda run -n multi-sem --no-capture-output \
+    python -u ./2-planes-invmap.py $data_loader $basepath $top $patch_size $stride`
+jobid=`expr match "$bsub_stdout" "$jobid_regex"`
+dependency=done\($jobid\)
+
+bsub_flags=(-Phess -n8 -W 1440)
+logfile=$basepath/warp-$params.log
+bsub_stdout=`bsub ${bsub_flags[@]} -w "$dependency" -oo $logfile \
+    conda run -n multi-sem --no-capture-output \
+    python -u ./2-planes-warp.py $data_loader $basepath $top $bot $patch_size $stride $chunk`
