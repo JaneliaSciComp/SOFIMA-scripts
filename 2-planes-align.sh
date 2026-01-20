553a49353fcccc3211f043b01a5e5075924f6e4c
Ben Arthur <arthurb@hhmi.org>
2026-01-20 13:58:46 -0500

    adjust compute resources needed for 2-planes

--- 2-planes-align.sh
+++ 2-planes-align.sh
@@ -1,6 +1,6 @@
 # ./2-planes-align.sh <data-loader> <basepath> <patch-size> <stride> <batch-size> <chunk>
 
-# ./2-planes-align.sh data-hess-2-planes /nrs/hess/data/hess_wafers_60_61/export/zarr_datasets/surface-align/run_20251219_110000/pass03-scale2 flat-w61_serial_080_to_089-w61_s080_r00-top-face.zarr flat-w61_serial_070_to_079-w61_s079_r00-bot-face.zarr 160 8 1024 1024
+# ./2-planes-align.sh data-hess-2-planes /nrs/hess/data/hess_wafers_60_61/export/zarr_datasets/surface-align/run_20251219_110000/pass03-scale2 flat-w61_serial_080_to_089-w61_s080_r00-top-face.zarr flat-w61_serial_070_to_079-w61_s079_r00-bot-face.zarr 80 8 1024 1024
 
 data_loader=$1
 basepath=$2
@@ -11,11 +11,11 @@ stride=$6
 batch_size=$7
 chunk=$8
 
-params=patch${patch_size}.stride${stride}
+params=patch${patch_size}.stride${stride}.top${top}
 
 jobid_regex='Job <\([0-9]*\)> '
 
-bsub_flags=(-Phess -n4 -gpu "num=1" -q gpu_l4 -W 1440)
+bsub_flags=(-Phess -n1 -gpu "num=1" -q gpu_l4 -W 1440)
 logfile=$basepath/flow-mesh-$params.log
 bsub_stdout=`bsub ${bsub_flags[@]} -oo $logfile \
     conda run -n multi-sem --no-capture-output \
@@ -23,7 +23,7 @@ bsub_stdout=`bsub ${bsub_flags[@]} -oo $logfile \
 jobid=`expr match "$bsub_stdout" "$jobid_regex"`
 dependency=done\($jobid\)
 
-bsub_flags=(-Phess -n32 -W 1440)
+bsub_flags=(-Phess -n2 -W 1440)
 logfile=$basepath/invmap-$params.log
 bsub_stdout=`bsub ${bsub_flags[@]} -w "$dependency" -oo $logfile \
     conda run -n multi-sem --no-capture-output \
@@ -37,7 +37,7 @@ dependency=done\($jobid\)
 #    conda run -n multi-sem --no-capture-output \
 #    python -u ./2-planes-warp.py $data_loader $basepath $top $bot $patch_size $stride $chunk`
 
-bsub_flags=(-Phess -n8 -W 1440)
+bsub_flags=(-Phess -n8 -W 1440)   # only needs -n1 for RAM but will use more CPU if given
 logfile=$basepath/warp-$params.log
 bsub_stdout=`bsub ${bsub_flags[@]} -w "$dependency" -oo $logfile \
     julia -t auto ./2-planes-warp.jl ${data_loader}.jl $basepath $top $bot $patch_size $stride $chunk`
